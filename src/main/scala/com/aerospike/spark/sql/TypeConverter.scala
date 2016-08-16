dd669ae6b0758bb70b977680ef1487d5e19931df
Peter Milne <helipilot50@gmail.com>
2016-08-16 19:50:43 +0200

    Added support to read and write Lists

--- src/main/scala/com/aerospike/spark/sql/TypeConverter.scala
+++ src/main/scala/com/aerospike/spark/sql/TypeConverter.scala
@@ -1,25 +1,25 @@
 package com.aerospike.spark.sql
 
+import scala.collection.mutable.WrappedArray
+
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.ArrayType
 import org.apache.spark.sql.types.BinaryType
 import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.types.DateType
 import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.types.FloatType
 import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.types.LongType
 import org.apache.spark.sql.types.MapType
+import org.apache.spark.sql.types.NullType
 import org.apache.spark.sql.types.ShortType
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StructType
+import collection.JavaConverters._
 
 import com.aerospike.client.Bin
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.DateType
-import org.apache.spark.sql.types.NullType
 
 /**
  * This object provides utility methods to convert between
@@ -45,10 +45,17 @@ object TypeConverter{
 		val binVal = bin._2
 		val binName = bin._1
 		//println(s"binToValue - name:$binName = value:$binVal")
+		
     val value = schema(binName).dataType match {
 		  case _: LongType => if (binVal == null) null else binVal.asInstanceOf[java.lang.Number].longValue
 		  case _: IntegerType => if (binVal == null) null else binVal.asInstanceOf[java.lang.Number].intValue
 		  case _: DoubleType => if (binVal == null) null else binVal.asInstanceOf[java.lang.Number].doubleValue()
+		  case _: ArrayType => {
+		    if (binVal == null) null else 
+		      //println(binVal.getClass.getCanonicalName)
+		      binVal
+		  }
+		  case _: MapType => if (binVal == null) null else binVal.asInstanceOf[java.util.Map[Any,Any]]
 		  case null => null
 		  case _ => if (binVal == null) null else binVal.toString()
 		}
@@ -62,6 +69,7 @@ object TypeConverter{
 		  Bin.asNull(field)
 		} else {
       schema(field).dataType match {
+        case StringType => new Bin(field, value)
   		  case LongType => new Bin(field, value.asInstanceOf[java.lang.Long])
   		  case IntegerType => new Bin(field, new java.lang.Long(value.asInstanceOf[Int]))
   		  case ShortType => new Bin(field, value.asInstanceOf[java.lang.Long])
@@ -69,8 +77,14 @@ object TypeConverter{
   		  case FloatType => new Bin(field, value.asInstanceOf[java.lang.Double])
   		  case DateType => new Bin(field, value.asInstanceOf[java.sql.Date].getTime)
   		  case NullType => Bin.asNull(field)
-  		  case _: ArrayType => new Bin(field, value)
-  		  case _: MapType => new Bin(field, value)
+  		  case _: ArrayType => {
+  		    value match {
+  		      case _: WrappedArray[Any] => new Bin(field, value.asInstanceOf[WrappedArray[Any]].toList.asJava)
+  		      case _: Seq[Any] => new Bin(field, scala.collection.JavaConversions.seqAsJavaList(value.asInstanceOf[Seq[Any]]))
+  		      case _: List[Any] => new Bin(field, value)
+  		    }
+  		  }
+  		  case _: MapType => new Bin(field, value.asInstanceOf[java.util.Map[Any, Any]])
   		  case null => Bin.asNull(field)
   		  case _ => new Bin(field, value.toString())
         }
@@ -88,15 +102,15 @@ object TypeConverter{
 					case _: java.lang.Double => StructField(binName, DoubleType, nullable = true)
 					case _: java.lang.Float => StructField(binName, DoubleType, nullable = true)
 					case s: String => StructField(binName, StringType, nullable = true)
-					case Map => {
+					case _: java.util.Map[Object,Object] => {
 					  val aKey = valueToSchema((binName, binVal.asInstanceOf[Map[Object, Object]].keys.head))
 					  val aValue = valueToSchema((binName, binVal.asInstanceOf[Map[Object, Object]].values.head))
 					  StructField(binName, new MapType(aKey.dataType, aValue.dataType, true), nullable = true) 
 					}
-					case List => { 
+					case _: java.util.List[Object] => { 
 					  val newValue = binVal.asInstanceOf[java.util.List[Object]].get(0)
 					  val elementStructure = valueToSchema((binName, newValue))
-					  StructField(binName, new ArrayType(elementStructure.dataType , true), nullable = true) 
+					  StructField(binName, new ArrayType(elementStructure.dataType , true)) 
 					}
 					//case ParticleType.GEOJSON => StructField(binName, StringType, nullable = true) //TODO 
 					case Array => StructField(binName, BinaryType, nullable = true)
